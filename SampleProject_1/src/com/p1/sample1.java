fddd3c264433bcded782e485e41722ba023014c1
Vishnu <vishnu3121999@gmail.com>
2021-08-05 15:30:08 +0530

    This is first commit

---
+++ SampleProject_1/src/com/p1/sample1.java
@@ -0,0 +1,11 @@
+package com.p1;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to git");
+System.out.println("this sample1 class");
+	}
+
+}
